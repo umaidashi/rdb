1011b306ea2e3a45a2caf2aa666faf1ec004736b
umaidashi <umaidashi18@gmail.com>
2024-08-09 23:48:37 +0900

    feat: count query

--- sqlap/01_jaywalk/NG/postgres/scripts/select.sql
+++ sqlap/01_jaywalk/NG/postgres/scripts/select.sql
@@ -15,3 +15,24 @@ FROM
   LEFT JOIN teams t on m.id = ANY(string_to_array(t.member_id, ',')::INT[])
 WHERE
   m.id = 1;
+
+-- チームに所属するメンバー数を集計
+SELECT
+  t.name as teams,
+  count(m.id) as members
+FROM
+  teams t
+  LEFT JOIN members m on m.id = ANY(string_to_array(t.member_id, ',')::INT[])
+GROUP BY
+  t.name;
+
+-- メンバーが所属するチーム数を集計
+SELECT
+  m.name as members,
+  count(t.id) as teams
+FROM
+  members m
+  LEFT JOIN teams t on m.id = ANY(string_to_array(t.member_id, ',')::INT[])
+GROUP BY
+  m.name;
+
